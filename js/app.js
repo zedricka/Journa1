4cdb880e4aa6de0a4093d43e4b623f152bc9f274
zedricka <zedrick.ara@gmail.com>
2017-03-15 09:07:22 -1000

    WorkingOnJSON

---
+++ js/app.js
@@ -0,0 +1,61 @@
+var jsonData = [
+  {
+    "header": [{
+    "div": {
+      "class": "container",
+      ["div": {
+        "class": "colortext",
+        ["center", "h1", "h4" "img": {
+          "class": "spooky"
+        }]
+      }]
+    }
+    }
+  },
+];
+
+var entry1 = [
+  {
+    "div": {
+      "id": "first",
+      [{"div": 
+        "class": "article",
+        [{"img":
+        "class": "succ", "src": "http://image.prntscr.com/image/6731b69cf0fc4dcb92d4f4c6408018ca.png", "margin-right":20px,}]
+        "h2", "h6", "h3", "i", "p", "b", "span":
+        "class" "highlight"
+      }]
+    }
+  }]
+
+  var journalEntries = [
+  {
+    "journalEntry": "1",
+    "date": "Aug 17, 2016",
+    "title": "Why I took this Class",
+    "content": "Because I'm interested in coding/programming in general and I want to learn more about it. What I want to get out of this class. To learn coding and to expand out from it."
+    "img": "http://image.prntscr.com/image/6731b69cf0fc4dcb92d4f4c6408018ca.png"
+  },
+  {
+    "journalEntry": "2",
+    "date": "Aug 24, 2016",
+    "title": "Difference Between UX and UI",
+    "content": ["UX stands for User Experience. This is how the user or person is enjoying your content/product. This usually is how the user is taking in the UI of your product and how they appeal to it. A good UX means a good UI!",
+     "UI stands for User Interface. This is how your product looks like visually to the user. Usually, you would want your product to be appealing to the user so the UI would have to look neat and clean."]
+     "img": "http://i.makeagif.com/media/5-26-2015/WVOq4L.gif"
+  },
+  {
+    "journalEntry": "3",
+    "date": "Aug 31, 2016",
+    "title": "What I thought of HTML & CSS",
+    "content": "I think CSS is a cool way of organizing your work and to make your coding look more clean. You can have a seperate tab of all of the background color, font size, font color, etc seperated from the main structure of your code.",
+    "img": "https://lh3.googleusercontent.com/SRJHeg-WfYjoHxrppj2mXYYxhrsfFnoQgWhNHdW2g4pMYbrnUz05FRdInRvLcZJwA-hL=w368-h207-p-no"
+  }
+  {
+    "journalEntry": "4",
+    "date": "Oct 7, 2016",
+    "title": "What I thought about Digimon Go",
+    "content": "The Digimon Go Activity was a bit challenging for me at first, since I did not know how to do the middle part of the blog. In this assignment I learned how to use borders, divs and other stuff more properly."
+    "img": "https://media0.giphy.com/media/FrHN5ahAJ6GZO/200.gif#6"
+  }
+]
